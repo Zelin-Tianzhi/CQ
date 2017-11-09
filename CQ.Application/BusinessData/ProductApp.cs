b07aff1f57b14541246422573e8377e3e4cb8c3b
泽林 <walkingfy@gmail.com>
2017-11-09 21:32:28 +0800

    添加图片上传插件

--- CQ.Application/BusinessData/ProductApp.cs
+++ CQ.Application/BusinessData/ProductApp.cs
@@ -39,7 +39,6 @@ namespace CQ.Application.BusinessData
             }
             else
             {
-                areaEntity.Create();
                 service.Insert(areaEntity);
             }
         }
--- CQ.Permission/Areas/ContentManage/Controllers/GamesController.cs
+++ CQ.Permission/Areas/ContentManage/Controllers/GamesController.cs
@@ -1,7 +1,10 @@
 ﻿using CQ.Application.BusinessData;
 using CQ.Core;
+using CQ.Domain.Entity.BusinessData;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -31,6 +34,52 @@ namespace CQ.Permission.Areas.ContentManage.Controllers
         {
             var data = productApp.GetForm(keyValue);
             return Content(data.ToJson());
+        }
+
+        [HttpPost]
+        [HandlerAjaxOnly]
+        [ValidateAntiForgeryToken]
+        [ValidateInput(false)]
+        public ActionResult SubmitForm(ProductEntity productEntity, string keyValue)
+        {
+            productEntity.Create();
+            productApp.SubmitForm(productEntity, keyValue);
+            return Success("操作成功。");
+        }
+
+        public JsonResult UploadImg()
+        {
+            var oFile = Request.Files["txt_file"];
+
+            var oStream = oFile.InputStream;
+            //得到了文件的流对象，我们不管是用NPOI、GDI还是直接保存文件都不是问题了吧。。。。
+
+            //后台TODO
+
+            try
+            {
+                byte[] bytes = new byte[oStream.Length];
+                oStream.Read(bytes, 0, bytes.Length);
+                oStream.Seek(0, System.IO.SeekOrigin.Begin);
+                Session["img1"] = bytes;
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            System.Drawing.Image img = System.Drawing.Bitmap.FromStream(oStream);
+            Bitmap bmp = new Bitmap(img);
+            MemoryStream bmpStream = new MemoryStream();
+            bmp.Save(bmpStream, System.Drawing.Imaging.ImageFormat.Jpeg);
+            string fileName = "";
+            FileStream fs = new FileStream(System.Web.HttpContext.Current.Server.MapPath("~/b1.jpg"), FileMode.Create);
+            bmpStream.WriteTo(fs);
+            bmpStream.Close();
+            fs.Close();
+            bmpStream.Dispose();
+            fs.Dispose();
+            return Json(new { success = true, msg = 0 }, JsonRequestBehavior.AllowGet);
         }
     }
 }
