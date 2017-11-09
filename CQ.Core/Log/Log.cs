3d4d608b7177fe18e6f4936286f846cc370b6cf9
泽林 <walkingfy@gmail.com>
2017-11-09 12:23:02 +0800

    补全遗漏Log

---
+++ CQ.Core/Log/Log.cs
@@ -0,0 +1,51 @@
+﻿/*******************************************************************************
+ * Copyright © 2017 Zl 版权所有
+ * Author: Zl
+ * Description: Tz通用权限
+*********************************************************************************/
+using System;
+using log4net;
+
+namespace CQ.Core
+{
+    public class Log
+    {
+        private ILog logger;
+        public Log(ILog log)
+        {
+            this.logger = log;
+        }
+        public void Debug(object message)
+        {
+            this.logger.Debug(message);
+        }
+        public void Debug(object message,Exception ex)
+        {
+            this.logger.Debug(message,ex);
+        }
+        public void Error(object message)
+        {
+            this.logger.Error(message);
+        }
+        public void Error(object message, Exception ex)
+        {
+            this.logger.Error(message,ex);
+        }
+        public void Info(object message)
+        {
+            this.logger.Info(message);
+        }
+        public void Info(object message, Exception ex)
+        {
+            this.logger.Info(message,ex);
+        }
+        public void Warn(object message)
+        {
+            this.logger.Warn(message);
+        }
+        public void Warn(object message, Exception ex)
+        {
+            this.logger.Warn(message,ex);
+        }
+    }
+}
