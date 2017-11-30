28fad75288671bc7e8163997ce99c43e049cfa3c
泽林 <walkingfy@gmail.com>
2017-11-30 23:06:39 +0800

    充值模块

---
+++ CQ.Permission/Areas/RechargeSystem/RechargeSystemAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace CQ.Permission.Areas.RechargeSystem
+{
+    public class RechargeSystemAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "RechargeSystem";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "RechargeSystem_default",
+                "RechargeSystem/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
