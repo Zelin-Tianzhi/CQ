a56e29a60c4efefc2938f5916731b137ddcd9e0e
zelin <walkingfy@gmail.com>
2017-12-11 17:46:59 +0800

    房间AI

---
+++ CQ.Permission/Areas/SystemConfig/Controllers/RoomAiController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CQ.Permission.Areas.SystemConfig.Controllers
+{
+    public class RoomAiController : BaseController
+    {
+        #region 属性
+
+
+
+        #endregion
+
+        #region 视图
+
+
+
+        #endregion
+
+        #region Ajax请求
+
+        
+
+        #endregion
+    }
+}
